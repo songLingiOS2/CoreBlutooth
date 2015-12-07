d8df227052bbe5190485f63bc646ad1861dd5308
yd <yaode@yddeiMac.local>
2015-12-07 16:43:28 +0800

    第一版

---
+++ new_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  new_ViewController.h
+//  
+//
+//  Created by yd on 15/12/7.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface new_ViewController : UIViewController
+
+@end
---
+++ new_ViewController.m
@@ -0,0 +1,37 @@
+//
+//  new_ViewController.m
+//  
+//
+//  Created by yd on 15/12/7.
+//
+//
+
+#import "new_ViewController.h"
+
+@interface new_ViewController ()
+
+@end
+
+@implementation new_ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
